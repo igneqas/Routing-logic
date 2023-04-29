107f94b0436832a2a3ed56a890c50d09aadd8998
IgnasK <ignask456@gmail.com>
2023-04-29 17:20:34 +0300

    Fixed bug

--- src/main/java/com/routerbackend/Constants.java
+++ src/main/java/com/routerbackend/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
     public static final int MEMORY_CLASS = 128;
     public static final int MAXNODES_ISLAND_CHECK = 500;
     public static final double GRAVITY = 9.81;
-    public static final double BIKER_POWER = 0.0;
-    public static final double ROAD_RESISTANCE = 0.0;
+    public static final double BIKER_POWER = 100.f;
+    public static final double ROAD_RESISTANCE = 0.01f;
 }
